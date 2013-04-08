ada0cf1a8ef380bff0ca0ada567401ea14a25089
Michael Rojas <mrojas@stevens.edu>
2013-04-07 21:15:39 -0300

    Update AppConfig.php
    Make AppConfig class a singleton.

--- AppConfig.php
+++ AppConfig.php
@@ -1,68 +1,66 @@
-<?php
-class AppConfig 
-{
-  static private $debug = true;
-
-  static private $env;
-
-	static private $navs;
-
-	static private $mode;
-
-	static public function getEnv() {
-		if(!isset(self::$env)) {
+<?php #AppConfig.php
+class AppConfig {
+	private $env;
+	private $navs;
+	private $mode;
+	private $debug;
+	
+	public static function getInstance(){
+		static $instance = NULL;
+		if($instance === NULL){
 			$configPath = dirname(__FILE__).'/../appconfig.xml';
-			if (file_exists($configPath)) {
-			    $xml = simplexml_load_file($configPath);
-			    $mode = (string)$xml->mode;
-			    self::$mode = $mode;
-			    self::$env = $xml->$mode;
-			    self::$debug = (string)self::$env->debug === 'true';
-			    if(isset(self::$env->web)) {
-			    	self::$navs = self::navsToArray(self::$env->web->nav);
-			    }
-			} else {
-			    exit('Application Error occured - code 505');
-			}
+			$instance = new AppConfig($configPath);
 		}
-		return self::$env;
+		return $instance;
+	}
+	
+	private function __construct(&$configPath){
+		if(!is_readable($configPath)){
+			exit('Application Error occurred - code 505');
+		}
+		$xml = simplexml_load_file($configPath);
+		$mode = (string)$xml->mode;
+		$this->mode = $mode;
+		$this->env = $xml->$mode;
+		$this->debug = (string)strtolower($this->env->debug) === 'true';
+		$this->navs = (isset($this->env->web))? self::navsToArray($this->env->web->nav): NULL;
+		return $this->env;
 	}
 
-	static public function getDebug() {
-		return self::$debug;
+	public function getDebug() {
+		return $this->debug;
 	}
 
-	static public function getNavs() {
-		if(!isset(self::$navs))
-			self::getEnv();
-		return self::$navs;
+	public function getNavs() {
+		return $this->navs;
 	}
 
-	static function navsToArray($xmlNav) {
+	private function navsToArray(&$xmlNav){
 		$nav = array();
-		if(isset($xmlNav)){
-			foreach ($xmlNav->children() as $role => $navitem) {
-				$rootlink = $xmlNav->$role->children();
-				$roleLinks = array();
-				$roleName = (string) $role;
-				foreach ($rootlink as $v => $links) {
-					$links = $links->attributes();
-					$name = (string)$links["name"];
-					$home = (string)self::$env->home;
-					$path = (string)$links["path"];
-					$url = (string)$links['prepend'] ? ($home).$path :$path;
-					$roleLinks[$name] = $url;
-				}
-				$nav[$roleName] = $roleLinks;
+		
+		$home = (string)$this->env->home;
+		foreach($xmlNav->children() as $role => $navitem){
+			$rootlink = $xmlNav->$role->children();
+			$roleLinks = array();
+			foreach($rootlink as $v => $links){
+				$links = $links->attributes();
+				$name = (string)$links['name'];
+				$path = (string)$links['path'];
+				$url = ($links['prepend'])? $home . $path: $path;
+				$roleLinks[$name] = $url;
 			}
-			if(isset($nav['all'])) {
-				foreach ($nav as $key => $value) {
-					if($key !== 'all') {
-						$nav[$key] = array_merge($nav['all'], $nav[$key]);
-					}
+			$roleName = (string)$role;
+			$nav[$roleName] = $roleLinks;
+		}
+		
+		if(isset($nav['all'])) {
+			foreach($nav as $key => $value){
+				if($key !== 'all'){
+					$nav[$key] = array_merge($nav['all'], $nav[$key]);
 				}
 			}
 		}
+		
 		return $nav;
 	}
 }
